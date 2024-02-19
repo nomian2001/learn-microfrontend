338286a669de7e58205afd3b28b35ccfa5d6b8d9
nomian2001 <thanhtung2833@gmail.com>
2024-02-19 11:37:12 +0700

    feat: update pipeline

--- container/src/App.js
+++ container/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
+  console.log('update yml')
     return (
        <div>
          <h1>Contanier Hi!!</h1>
