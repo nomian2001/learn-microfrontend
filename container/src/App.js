73a608043fd2f1455e6726086286651f5d6103ae
nomian2001 <thanhtung2833@gmail.com>
2024-02-19 15:15:08 +0700

    feat: test pipeline

--- container/src/App.js
+++ container/src/App.js
@@ -2,11 +2,9 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
-  console.log('co gi hong');
-
     return (
        <div>
-         <h1>Contanier Hi!!</h1>
+         <h1>Contanier Hi le!!</h1>
          <hr />
          <MarketingApp /> 
        </div>
