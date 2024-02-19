adedbfb6d663f2fd96bda29038674632bfc25b13
nomian2001 <thanhtung2833@gmail.com>
2024-02-19 11:19:20 +0700

    feat: test pipeline

--- container/src/App.js
+++ container/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
+  console.log('alo');
+
     return (
        <div>
          <h1>Contanier Hi!!</h1>
