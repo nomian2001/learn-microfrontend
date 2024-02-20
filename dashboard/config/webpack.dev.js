8d946fcfa5c62f7b3e4762579cd6d9f717fcf33c
nomian2001 <thanhtung2833@gmail.com>
2024-02-20 15:25:12 +0700

    feat: update dashboard with vuejs

--- dashboard/config/webpack.dev.js
+++ dashboard/config/webpack.dev.js
@@ -4,28 +4,30 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
+
 const devConfig = {
     mode: 'development',
-    output: {
-        publicPath: 'http://localhost:8080/'
+  output: {
+    publicPath: 'http://localhost:8083/',
+  },
+  devServer: {
+    port: 8083,
+    historyApiFallback: {
+      historyApiFallback: true,
     },
-    devServer: {
-        port: 8080,
-        historyApiFallback: {
-            index: '/index.html'
-        }
+    headers: {
+      'Access-Control-Allow-Origin': '*',
     },
+  },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'container',
-            remotes: {
-                marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-                auth: 'auth@http://localhost:8082/remoteEntry.js',
-                dashboard: 'dashboard@http://localhost:8083/remoteEntry.js'
+            name: 'dashboard',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './App': './src/bootstrap'
             },
             shared: packageJson.dependencies
-
-        }),
+        })
     ]
 }
 
--- dashboard/config/webpack.prod.js
+++ dashboard/config/webpack.prod.js
@@ -1,9 +1,6 @@
 const {merge} = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
-
-const domain = process.env.PRODUCTION_DOMAIN;
-
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
@@ -12,19 +9,17 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/container/latest/'
+        publicPath: '/dashboard/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'container',
-            remotes: {
-                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-                auth: `auth@${domain}/auth/latest/remoteEntry.js`,
-                dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`
-
+            name: 'dashboard',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './App': './src/bootstrap'
             },
             shared: packageJson.dependencies
-        }),
+        })
     ]
 }
 
